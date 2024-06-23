84f9d5f61cc48b292253800d33abceed8bd6ce69
peaceissa <issarae78@gmail.com>
2024-06-23 21:40:39 +0300

    added dcoker file

--- Version_2/index.php
+++ Version_2/index.php
@@ -3,7 +3,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     // Process the login here (e.g., check credentials in a database)
-    echo "Username: " . htmlspecialchars($username) . "<br>";
-    echo "Password: " . htmlspecialchars($password) . "<br>";
+    if ($username == 'admin' && $password == 'admin123') {
+        echo 'Welcome admin! ';
+    } else {
+        echo 'Invalid username or password';
+    }
  }
  ?>
